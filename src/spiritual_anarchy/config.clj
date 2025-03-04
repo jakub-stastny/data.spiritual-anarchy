4c6697719fbea35b2ac331e9cada14d079f21e3e
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2025-03-04 06:18:40 -0600

    Use feed path; formatting

--- src/spiritual_anarchy/config.clj
+++ src/spiritual_anarchy/config.clj
@@ -1,6 +1,7 @@
 (ns spiritual-anarchy.config
   (:require [clojure.edn :as edn]
-            [fipp.edn :as fipp]))
+            [fipp.edn :as fipp]
+            [cheshire.core :as json]))
 
 (def feed-path "data/feed.edn")
 (def feed (edn/read-string (slurp feed-path)))
@@ -8,13 +9,21 @@
 (defn pretty-format [data]
   (with-out-str (fipp/pprint data)))
 
+(def format-json json/generate-string)
+
 (defn add-to-feed [item]
   (spit feed-path (pretty-format (into [] (conj feed item)))))
 
-(def json-feed-path "data/feed.json")
+(def json-feed-path "feed/posts.json")
+
+(defn write-json-feed [feed]
+  (spit json-feed-path (format-json feed)))
 
 (defn tag-feed-path [tag]
-  (str "data/tags/" tag ".json"))
+  (str "feed/tags/" tag ".json"))
+
+(defn write-tag-feed [tag feed]
+  (spit (tag-feed-path tag) (format-json feed)))
 
 (defn get-user-input
   ([prompt]
