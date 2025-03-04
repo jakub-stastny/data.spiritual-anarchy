7a1d5a3d18c665498744c2d434d35632dbeeccb8
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2025-03-04 04:08:55 -0600

    Pretty-print using fipp

--- src/spiritual_anarchy/config.clj
+++ src/spiritual_anarchy/config.clj
@@ -1,11 +1,15 @@
 (ns spiritual-anarchy.config
-  (:require [clojure.edn :as edn]))
+  (:require [clojure.edn :as edn]
+            [fipp.edn :as fipp]))
 
 (def feed-path "data/feed.edn")
 (def feed (edn/read-string (slurp feed-path)))
 
+(defn pretty-format [data]
+  (with-out-str (fipp/pprint data)))
+
 (defn add-to-feed [item]
-  (spit c/feed-path (pr-str (conj feed item))))
+  (spit feed-path (pretty-format (into [] (conj feed item)))))
 
 (def json-feed-path "data/feed.json")
 
