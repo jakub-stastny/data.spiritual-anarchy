987d971e7f96c44997004efedb933c7d99346b90
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2025-03-04 06:36:18 -0600

    Switch branches

--- src/spiritual_anarchy/tools/generate_runner.clj
+++ src/spiritual_anarchy/tools/generate_runner.clj
@@ -1,13 +1,18 @@
 (ns spiritual-anarchy.tools.generate-runner
   (:require [clojure.string :as str]
             [spiritual-anarchy.config :as c]
-            [babashka.fs :as fs]
-            [babashka.process :as process]))
-
-;; TODO: Switch branches
+            [babashka.process :refer [shell]]))
 
 (defn -main []
+  (try
+    (shell "git checkout generated-data")
+    (catch Exception error
+      (System/exit 1)))
+
   (c/write-json-feed c/feed)
 
   (doseq [[tag posts] (c/group-feed-by-tags)]
-    (c/write-tag-feed tag posts)))
+    (c/write-tag-feed tag posts))
+
+  (shell "git" "commit" "-m" "Regenerated data")
+  (shell "git checkout master"))
